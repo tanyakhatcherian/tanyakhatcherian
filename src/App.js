7a5c6ef5282a13a7dfad763375291f09f54b4eef
tanyakhatcherian <tanyakhatcherian@hotmail.ca>
2022-04-19 11:57:29 -0400

    react-test-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import TempComp from './TempComp';
 
-function App() {
+//hisam i found this quotes api snippet of code online that was relatively easy to use and understand so i just styled it myself, removed the twitter function and i wrote comments under the code so you know that i kinda understand whats going on 
+
+//ref: https://github.com/kritika27/quotes-generator-react/blob/master/src/App.js
+
+
+// random quote generator
+const url = "https://api.quotable.io/random";
+
+// use state, quote is the data
+  const App = () => {
+  const [quotes, setQuotes] = useState([]);
+
+  // function getting the quote using fetch similar to how you showed in class except the url is a const defined above instead
+  const getQuote = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setQuotes(data));
+  };
+
+  //runs the api within the getQuote function with useEffect
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  //new Quote when button is pressed which is seen under <button onClick={getNewQuote}
+  const getNewQuote = () => {
+    getQuote();
+  };
+  
+  // the content will be inputted into the <p> underneath and that will be the quote, and then the author will pull the authors name under h5
+  const { content, author } = quotes;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Inspiring Quotes, brought to you by Tanya Khatcherian's React App</h1>
+      <TempComp/>
+    <div className="box-centerside">
+      <div className="text">
+        <p>{content}</p>
+      </div>
+      <div className="author">
+        <h5>{author}</h5>
+        <div className="button-container">
+          <Button onClick={getNewQuote}>New Quote</Button>
+        </div>
+      </div>
+    </div>
+    <h1>Hope you have a wonderful day!</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TempComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TempComp extends Component
+{
+    render() {
+        return(
+        <div><h3>hi Hisam, this is showing the link to the app component</h3></div>
+        )
+    }
+}
+
+export default TempComp;
